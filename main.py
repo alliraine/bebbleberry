22b6eb90c288ef76c06feec4b37ed4d339fb0fbe
Allison Chapman <achapman@digitalocean.com>
2023-05-27 22:02:32 -0400

    Add support for loading apps from disk

--- main.py
+++ main.py
@@ -1,69 +1,53 @@
+import logging
 import os
 
+import yaml
 from pyray import *
 from PIL import Image
-import PIL.ImageOps
 
 s_width = 400
 s_height = 240
 
 selected = 0
 
-apps = [
-    {
-        "name": "Beeper",
-        "icon": "cog-solid",
-    },
-    {
-        "name": "BlueSky",
-        "icon": "cloud-solid",
-    },
-    {
-        "name": "Weather",
-        "icon": "cloud-sun-rain-solid",
-    },
-    {
-        "name": "Settings",
-        "icon": "cog-solid",
-    },
-    {
-        "name": "Calendar",
-        "icon": "calendar-solid",
-    },
-    {
-        "name": "Notes",
-        "icon": "sticky-note-solid",
-    },
-    {
-        "name": "Terminal",
-        "icon": "terminal-solid",
-    },
-    {
-        "name": "App Store",
-        "icon": "download-solid",
-    },
-]
+def get_installed_apps():
+    apps = []
+    for app in [app for app in os.listdir("apps") if os.path.isdir(f"apps/{app}")]:
+        with open(f"apps/{app}/manifest.yml", "r") as stream:
+            try:
+                manifest = yaml.safe_load(stream)
+            except yaml.YAMLError:
+                print(f"{app} does not have a manifest!! Aborting load")
+        icon = load_texture(f"apps/{app}/res/launcher-icon.png")
+        inverse_icon = load_texture(f"apps/{app}/res/launcher-icon-inverse.png")
+        app = {
+            "name": manifest["name"],
+            "developer": manifest["developer"],
+            "version": manifest["version"],
+            "launch_file": manifest["launch_file"],
+            "icon": icon,
+            "inverse_icon": inverse_icon
+        }
+        apps.append(app)
+
+    return apps
 
 
 def main():
     global selected
+
+    ## create emulation window
     init_window(s_width, s_height, "BebbleBerry Emulator")
 
-    # create inverse of all icons
-    for file in os.listdir('icons'):
-        file = file.split(".")[0]
-        if "-inverse" not in file and file != "":
-            img = Image.open(f'icons/{file}.png')
-            r, g, b, a = img.split()
-            def invert(image):
-                return image.point(lambda p: 255 - p)
-            r, g, b = map(invert, (r, g, b))
-            img2 = Image.merge(img.mode, (r, g, b, a))
+    ## remove debug logs that spam
+    set_trace_log_level(logging.CRITICAL)
 
-            img2.save(f'icons/{file}-inverse.png')
+    ## loads apps from disk
+    apps = get_installed_apps()
 
     while not window_should_close():
-        # handle input
+
+        # handle input. kinda hacky. I think I can do better
         if is_key_pressed(264) and selected + 4 < len(apps):
             selected += 4
         if is_key_pressed(265) and selected - 4 >= 0:
@@ -78,6 +62,9 @@ def main():
 
         # draw notifications bar
         draw_rectangle(0, 0, s_width, 30, BLACK)
+
+        # draw push notification
+        draw_text("New Message From Ellie!", 8, 10, 10, WHITE)
 
         for app in apps:
 
@@ -95,30 +82,22 @@ def main():
                 x = i - 4
                 app_x = 8 + (x * 100) - a * x
 
+            # set colors based on if selected
+            bg_color = BLACK if i == selected else WHITE
+            text_color = WHITE if i == selected else BLACK
+            icon = app["inverse_icon"] if i == selected else app["icon"]
+
             # draw border
             draw_rectangle(app_x, app_y, 100, 100, BLACK)
-
-            #set colors
-            bg_color = WHITE
-            text_color = BLACK
-            icon = load_texture(f"icons/{app.get('icon')}.png")
-
-            if i == selected:
-                bg_color = BLACK
-                text_color = WHITE
-                icon = load_texture(f"icons/{app.get('icon')}-inverse.png")
-
 
             # draw background
             draw_rectangle(app_x + 5, app_y + 5, 90, 90, bg_color)
 
             # draw app name
-            draw_text(app.get("name"),app_x + int((90 - (len(app.get("name") * 6))) / 2), app_y + 80, 14, text_color)
+            draw_text(app.get("name"), app_x + int((90 - (len(app.get("name") * 6))) / 2), app_y + 80, 14, text_color)
 
+            # draw icon
             draw_texture(icon, app_x + 25, app_y + 15, text_color)
-
-        # push notification
-        draw_text("New Message From Ellie!", 8, 10, 10, WHITE)
 
         end_drawing()
     close_window()
